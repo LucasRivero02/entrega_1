eb9fa029d11d55edbbc92fc27ee892a4f033c47d
LucasRivero02 <112914005+LucasRivero02@users.noreply.github.com>
2022-09-23 18:28:57 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// aplique el uso de:
+// - declaracion de variables
+// - alert - prompt
+// - while
+// - if - switch
+// - funciones
+// - ademas adjunto el avance del html
+
+function proceso(){
+   let monedaOrigen = prompt("Ingrese la moneda origen\n1-Dolar\n2-Pesos\nESC-Salir");
+   while(monedaOrigen != "ESC" ){
+      var monedaCorrecta = false;
+      var monedaSimbolo;
+      var monedaSimboloSalida;
+      switch(monedaOrigen) {
+         case "1":
+            monedaCorrecta = true;
+            monedaSimbolo = 'U$S';
+            monedaSimboloSalida = '$';
+            break
+         case "2":
+            monedaCorrecta = true;
+            monedaSimbolo = '$';
+            monedaSimboloSalida = 'U$S';
+            break
+         default:
+            break;
+      }
+      if (monedaCorrecta){
+         let montoOrigen = parseInt(prompt('Ingrese monto en ' +  monedaSimbolo + ':'));
+         let cotizacion = parseInt(prompt('Ingrese cotizacion: '));
+         alert('Su monto cotizado es de: ' + monedaSimboloSalida + conversorMoneda(montoOrigen, cotizacion, monedaOrigen));
+      }else{
+         alert("Hay un error con la MONEDA ingresada");
+      }
+      monedaOrigen = prompt("Ingrese la moneda origen\n1-Dolar\n2-Pesos\nESC-Salir");
+   }
+   
+   function conversorMoneda(numeroA, numeroB, monedaOrigen) {
+      if(monedaOrigen === "1"){
+         return numeroA * numeroB;
+      }else{
+         return numeroA / numeroB;
+      }
+   }
+}
+
+proceso()
